3045afae950d53db3255bbd716f1f5b4b63a8672
Osman Taha Kıyıcılar <tr.osmantaha@gmail.com>
2021-08-30 11:13:19 +0300

    for-heroku_1

--- config-heroku.php
+++ config-heroku.php
@@ -8,7 +8,7 @@
 
 const Url = "http://www.koeri.boun.edu.tr/scripts/lst2.asp";
 const DB_host = "us-cdbr-east-04.cleardb.com";
-const DB_name = "b29808d3dde295";
-const DB_user = "heroku_fff230ad57fd176";
+const DB_name = "heroku_fff230ad57fd176";
+const DB_user = "b29808d3dde295";
 const DB_pass = "2e539844";
 const OnApiRequestCheckNewData = true;
